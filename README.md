bf355cb8b7f61d9e0e187d168d69e5992da67017
Hyeok Yoon <titania7777@gmail.com>
2020-10-01 08:58:35 +0000

    first

---
+++ README.md
@@ -0,0 +1,11 @@
+# UCF101 Frame Extractor with ray
+
+## Requirements
+
+*   ray>=0.8.7
+*   av>=8.0.2
+
+## Usage
+```
+frame_extractor.py --videos-path /path/to/videos --frames-path /path/to/save
+```
---
+++ frame_extractor.py
@@ -0,0 +1,32 @@
+import os
+import av
+import glob
+import ray
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--videos-path", type=str, default="./UCF101_videos/")
+parser.add_argument("--frames-path", type=str, default="./UCF101_frames/")
+args = parser.parse_args()
+
+ray.init(num_cpus=8)
+
+videos_path_list = glob.glob(args.video_path + '*.avi')
+print("will start extract frames from {} videos...".format(len(videos_path_list)))
+
+# check directory
+assert os.path.exists(args.frames_path) is False, "{} directory is alreay exist!!".format(args.frames_path)
+os.makedirs(args.frames_path)
+
+@ray.remote
+def extractor(index, video_path):
+    frame_name = video_path.split("\\" if os.name == 'nt' else "/")[-1].split('.avi')[0]
+    frame_path = os.path.join(args.frames_path, frame_name)
+    frames = [f.to_image() for f in av.open(video_path).decode(0)]
+    os.makedirs(frame_path)
+    for j, frame in enumerate(frames):
+        frame.save(os.path.join(frame_path, "{}.jpg".format(j)))
+    print("{}/{} extracted {} frames from '{}'".format(index, len(videos_path_list), len(frames), frame_name + '.avi'))
+
+features = [extractor.remote(i, video_path) for i, video_path in enumerate(videos_path_list)]
+ray.get(features)
