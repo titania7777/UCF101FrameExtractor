ac7165b41f15d273733cd0524cde6c30e32f8330
Hyeok Yoon <titania7777@gmail.com>
2020-10-22 14:48:01 +0000

    Modify

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # UCF101 Frame Extractor with ray
+this frame extractor supports optical flow(dense optical flow) also
 
 ## Requirements
 
 *   ray>=0.8.7
-*   av>=8.0.2
+*   opencv-python>=4.4.0.44
 
 ## Usage
 
-download raw videos [Download UCF101.tar](https://www.dropbox.com/s/xhwkilwgytox0j2/UCF101.tar?dl=0) and extract frames from videos
+download raw videos [Download UCF101.tar](https://www.dropbox.com/s/xhwkilwgytox0j2/UCF101.tar?dl=0) and decompress it
+
+extract only frames
 ```
 frame_extractor.py --videos-path /path/to/videos --frames-path /path/to/save --num-cpus /number/of/cpus
+```
+
+extract both (frames, flows)
+```
+frame_extractor.py --videos-path /path/to/videos --frames-path /path/to/save --flows-path /path/to/save --num-cpus /number/of/cpus --flow-mode
 ```
--- frame_extractor.py
+++ frame_extractor.py
@@ -1,34 +1,89 @@
 import os
-import av
-from glob import glob
 import ray
+import numpy as np
+from glob import glob
+import cv2
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--videos-path", type=str, default="../Data/UCF101/UCF101_videos/")
-parser.add_argument("--frames-path", type=str, default="../Data/UCF101/UCF101_frames/")
+parser.add_argument("--frames-path", type=str, default="./UCF101_frames/")
+parser.add_argument("--flows-path", type=str, default="./UCF101_flows/")
+parser.add_argument("--flow-mode", action="store_true")
 parser.add_argument("--num-cpus", type=int, default=8)
+parser.add_argument("--pyr-scale", type=float, default=0.5)
+parser.add_argument("--levels", type=int, default=3)
+parser.add_argument("--winsize", type=int, default=15)
+parser.add_argument("--iterations", type=int, default=3)
+parser.add_argument("--poly-n", type=int, default=5)
+parser.add_argument("--poly-sigma", type=float, default=1.2)
+parser.add_argument("--flags", type=int, default=0)
 args = parser.parse_args()
 
-# check directory
-assert os.path.exists(args.frames_path) == False, "'{}' directory is alreay exist !!".format(args.frames_path)
-os.makedirs(args.frames_path)
+# check directory(videos_path, frames_path, flows_path)
+assert os.path.exists(args.videos_path) is True, "'{}' directory is not exist !!".format(args.videos_path)
+assert os.path.exists(args.frames_path) is False, "'{}' directory is already exist !!".format(args.frames_path)
+assert os.path.exists(args.flows_path) is False, "'{}' directory is already exist !!".format(args.flows_path)
+
+# get videos path
+videos_path_list = glob(os.path.join(args.videos_path, "*"))
 
 # init ray on local
 ray.init(num_cpus=args.num_cpus)
 
-videos_path_list = glob(os.path.join(args.videos_path, "*.avi"))
-print("will be start extract frames from {} videos...".format(len(videos_path_list)))
-
 @ray.remote
-def extractor(index, video_path):
+def dense_optical_flow(index, video_path):
     frame_name = video_path.split("\\" if os.name == 'nt' else "/")[-1].split('.avi')[0]
+    
+    # frame path
     frame_path = os.path.join(args.frames_path, frame_name)
-    frames = [f.to_image() for f in av.open(video_path).decode(0)]
     os.makedirs(frame_path)
-    for j, frame in enumerate(frames):
-        frame.save(os.path.join(frame_path, "{}.jpg".format(j)))
-    print("{}/{} extracted {} frames from '{}'".format(index, len(videos_path_list), len(frames), frame_name + '.avi'))
+    
+    # read video
+    cap = cv2.VideoCapture(video_path)
+    ret, frame_first = cap.read()
+    if ret == False:
+        print("'{}' video read to fail !! skip this video...".format(video_path))
+        return
 
-features = [extractor.remote(i, video_path) for i, video_path in enumerate(videos_path_list)]
-ray.get(features)
+    length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    print("{}/{} name: {} length: {}".format(index+1, len(videos_path_list), frame_name, length))
+
+    # save first frame
+    if not cv2.imwrite(os.path.join(frame_path, "0.jpg"), frame_first):
+        raise Exception("could not write frame !!")
+
+    if args.flow_mode:
+        # flow path
+        flow_path = os.path.join(args.flows_path, frame_name)
+        os.makedirs(flow_path)
+
+        # convert to gray
+        frame_prev_gray = cv2.cvtColor(frame_first, cv2.COLOR_BGR2GRAY)
+        hsv = np.zeros_like(frame_first)
+        hsv[..., 1] = 255
+
+    for i in range(1, length):
+        # read next frame
+        ret, frame_next = cap.read()
+        if ret == False:
+            msg = "index '{}' of '{}' video read to fail !! skip this frame...".format(i, video_path)
+            errors.append(msg); print(msg)
+            continue
+
+        # save next frame 
+        if not cv2.imwrite(os.path.join(frame_path, "{}.jpg".format(i)), frame_next):
+            raise Exception("could not write frame !!")
+
+        if args.flow_mode:
+            frame_next_gray = cv2.cvtColor(frame_next, cv2.COLOR_BGR2GRAY)
+            # Computes a dense optical flow using the Gunnar Farneback's algorithm
+            frame_flow = cv2.calcOpticalFlowFarneback(frame_prev_gray, frame_next_gray, None, args.pyr_scale, args.levels, args.winsize, args.iterations, args.poly_n, args.poly_sigma, args.flags)
+            # Calculates the magnitude and angle of 2D vectors
+            mag, ang = cv2.cartToPolar(frame_flow[..., 0], frame_flow[..., 1])
+            hsv[..., 0] = ang*180/np.pi/2
+            hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+            cv2.imwrite(os.path.join(flow_path, "{}.jpg".format(i - 1)), cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR))
+            frame_prev_gray = frame_next_gray
+
+ray.get([dense_optical_flow.remote(i, video_path) for i, video_path in enumerate(videos_path_list)])
